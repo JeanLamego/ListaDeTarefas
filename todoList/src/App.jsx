7826bb64777825df835e22d742695c9f82638f5e
Jean Lamego <jeanlamego@hotmail.com>
2023-07-31 13:34:40 -0300

    projeto finalizado

--- todoList/src/App.jsx
+++ todoList/src/App.jsx
@@ -2,34 +2,43 @@ import { useState } from 'react'
 
 import Todo from './components/Todo';
 import TodoForm from './components/TodoForm'
+import Search from './components/Search';
+import Filter from './components/Filter';
 
 import './App.css';
 
 
+
+
 function App() {
   const [todos, setTodos] = useState([
-    
+
     {
-      id:1,
-      text: "criar funcionalidade x no sistema",
+      id: 1,
+      text: "Criar funcionalidade x no sistema",
       category: "Trabalho",
       isCompleted: false,
     },
     {
-      id:2,
+      id: 2,
       text: "Ir pra academia",
       category: "Pessoal",
       isCompleted: false,
     },
     {
-      id:3,
+      id: 3,
       text: "Estudar React",
       category: "Estudos",
       isCompleted: false,
     }
   ])
 
-  const addTodo = (text, category) =>{
+  const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
+
+  const addTodo = (text, category) => {
 
     const newTodos = [
       ...todos,
@@ -37,37 +46,47 @@ function App() {
         id: Math.floor(Math.random() * 10000),
         text,
         category,
-        isCompleted:false,
+        isCompleted: false,
       },
     ];
 
     setTodos(newTodos);
   }
 
-  const removeTodo= (id) =>{
+  const removeTodo = (id) => {
     const newTodos = [...todos];
-    const filteredTodos = newTodos.filter((todo)=>
-    todo.id !== id ? todo : null)
+    const filteredTodos = newTodos.filter((todo) =>
+      todo.id !== id ? todo : null)
     setTodos(filteredTodos);
   }
 
-  const completeTodo = (id) =>{
-    const newTodos=[...todos];
-    newTodos.map((todo)=> todo.id=== id ? todo.isCompleted = !todo.isCompleted : todo);
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo);
     setTodos(newTodos);
   };
-//React só poder ter um elemento pai, regra da bibilioteca
+  //React só poder ter um elemento pai, regra da bibilioteca
   return (
-  <div className="app">
-    <h1>Lista de Tarefas</h1>
-    <div className="todo-list">
-     {todos.map((todo) => (
-    <Todo key={todo.id}todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
+      <div className="todo-list">
+        {todos
+        .filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
+        .filter((todo) => 
+        todo.text.toLowerCase().includes(search.toLowerCase()))
+        .sort((a,b)=> sort==="Asc"
+         ? a.text.localeCompare(b.text) 
+         : b.text.localeCompare(a.text)
+        )
+        .map((todo) => (
+        <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
      ))}
+      </div>
+
+      <TodoForm addTodo={addTodo} />
     </div>
-    
-    <TodoForm addTodo={addTodo}/>
-  </div>
   );
 };
 
