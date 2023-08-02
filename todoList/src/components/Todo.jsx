f80a8fedb5d57c3e361c645d1177f6ceb4d7934c
Jean Lamego <jeanlamego@hotmail.com>
2023-08-01 21:01:47 -0300

    responsividade pronta

--- todoList/src/components/Todo.jsx
+++ todoList/src/components/Todo.jsx
@@ -8,7 +8,7 @@ const Todo = ({todo, removeTodo, completeTodo}) => {
         <p>{todo.text}</p>
         <p className="category">({todo.category})</p>
       </div>
-        <div>
+        <div className='todo-buttons'>
             <button className='complete' onClick={(() => completeTodo(todo.id))}>Completar</button>
             <button className='remove' onClick={(() => removeTodo(todo.id))}>X</button>
         </div>
